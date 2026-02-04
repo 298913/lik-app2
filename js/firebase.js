4f2247b62ac70c100149d424b1a145f67651e53c
298913 <122632312+298913@users.noreply.github.com>
2026-02-04 09:43:12 -0300

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
